d5d35158512f2ad656376b593c5908dfe085f481
Zeinab-Ayman <zeinabayman304@gmail.com>
2026-08-22 00:50:06 +0300

    feat(serv): add standard types and bit math libraries

---
+++ Serv/STD_TYPES.h
@@ -0,0 +1,17 @@
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+typedef unsigned char        uint8;
+typedef unsigned short int   uint16;
+typedef unsigned long int    uint32;
+typedef unsigned long long   uint64;
+
+typedef signed char          sint8;
+typedef signed short int     sint16;
+typedef signed long int      sint32;
+typedef signed long long     sint64;
+
+typedef float                f32;
+typedef double               f64;
+
+#endif /* STD_TYPES_H_ */
